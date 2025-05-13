4ab575739f2e7604fd4d17ff4fb21bc506d8330a
bbarclay <your-email@example.com>
2025-05-13 04:38:57 -0600

    all

--- src/content/api-reference/intelligent-messaging/_meta.js
+++ src/content/api-reference/intelligent-messaging/_meta.js
@@ -2,8 +2,6 @@ export default {
   index: "Overview",
   "message-details": "Message Details",
   "message-routes": "Message Routes",
-  "message-routes-create": "Create Route",
-  "message-routes-update": "Update Route",
   "message-routes-delete": "Delete Route",
   "outbound-message": "Send Outbound Message",
   "message-search": "Message Search"
